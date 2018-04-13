5bfe4c072b1f612029f375184af0dc4fe0d338c6
alan <falloonalan@gmail.com>
2018-04-13 21:29:21 +1200

    add solution and tests

--- src/marsrover.js
+++ src/marsrover.js
@@ -1,12 +1,15 @@
 
-function parseGrid(input) {
+
+
+
+
+function parseGrid(firstline) {
     try {
-        firstline = input.split('\n')[0];
         coords = firstline.split(' ');
-        x = parseInt(coords[0]);
-        y = parseInt(coords[1]);
-        
-        return [null, [x,y]];
+        x = Number(coords[0]);
+        y = Number(coords[1]);
+
+        return [null, [x, y]];
 
     } catch (err) {
         return ['Error parsing input: ' + err, null];
@@ -14,18 +17,134 @@ function parseGrid(input) {
     return ['Error parsing input', null];
 }
 
+function getRoverPosition(roverDescription) {
+    let input = roverDescription.split(' ');
 
-function readInput(input) {
-    
-    var [err, size] = parseGrid(input);
-    if (err) {
-        console.log(err);
+    if (input.length >= 3) {
+        let rover = {
+            x: Number(input[0]),
+            y: Number(input[1]),
+            dir: input[2]
+        }
+        return [null, rover]
     } else {
-        console.log(size);
+        return ['Error: Rover details must have at least 3 values', null]
     }
-
-    return -1;
 }
 
-exports.readInput=readInput
-exports.parseGrid=parseGrid
+
+function doRoverMoves(grid, rover, moves) {
+
+    if (!rover) {
+        return [`Error: doRoverMoves given ${rover} as rover`, null];
+    } else if (!moves) {
+        return [`Error: doRoverMoves given ${moves} for moves`, null];
+    }
+
+    for (let index = 0; index < moves.length; index++) {
+        const move = moves[index];
+
+        switch (move) {
+            case 'L':
+                switch (rover.dir) {
+                    case 'N':
+                        rover.dir = 'W';
+                        break;
+                    case 'E':
+                        rover.dir = 'N';
+                        break;
+                    case 'S':
+                        rover.dir = 'E';
+                        break;
+                    case 'W':
+                        rover.dir = 'S';
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            case 'R':
+                switch (rover.dir) {
+                    case 'N':
+                        rover.dir = 'E';
+                        break;
+                    case 'E':
+                        rover.dir = 'S';
+                        break;
+                    case 'S':
+                        rover.dir = 'W';
+                        break;
+                    case 'W':
+                        rover.dir = 'N';
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            case 'M':
+                switch (rover.dir) {
+                    case 'N':
+                        rover.y++;
+                        break;
+                    case 'E':
+                        rover.x++;
+                        break;
+                    case 'S':
+                        rover.y--;
+                        break;
+                    case 'W':
+                        rover.x--;
+                        break;
+                    default:
+                        break;
+                }
+
+                //TODO: checkOffGrid()
+
+                break;
+
+            default:
+                break;
+        }
+    }
+    return [null, rover];
+}
+
+function readInput(input) {
+
+    let output = '';
+    let lines = input.split('\n');
+    let [err, size] = parseGrid(lines[0]);
+    if (err != null) {
+        console.log(err);
+        return -1
+    }
+
+    for (let index = 1; index < lines.length - 1; index += 2) {
+        console.log(index, lines.length);
+
+        const roverDescription = lines[index];
+        const roverMoves = lines[index + 1];
+
+        let [err, rover] = getRoverPosition(roverDescription);
+        if (err != null) {
+            console.log(err);
+            return -1
+        }
+        [err, rover] = doRoverMoves(size, rover, roverMoves)
+        if (err != null) {
+            console.log(err);
+            return -1
+        }
+
+        output += `${rover.x} ${rover.y} ${rover.dir}\n`;
+    }
+
+    //remove the extra new line at the end
+    return output.trim();
+}
+
+exports.readInput = readInput
+exports.parseGrid = parseGrid
+exports.getRoverPosition = getRoverPosition
+exports.doRoverMoves = doRoverMoves
--- src/marsrover.test.js
+++ src/marsrover.test.js
@@ -6,36 +6,57 @@ const mars = require('./marsrover');
 const testinput1 = `5 5
 1 2 N
 LMLMLMLMM
-3 3 E`
+3 3 E
+MMRMMRMRRM`
 
 const testoutput1 = `1 3 N
 5 1 E`
 
-const testinput2 = `5 5
-1 2 N
-LMLMLMLMM
-3 3 E
-MMRMMRMRRM`
 
-const testoutput2 = `1 3 N
-5 1 E`
-
-describe('MarsRovermars', function() {
-  describe('Parse Input', function() {
-    it('should correctly parse the grid', function() {
-      var [err, grid] = mars.parseGrid('5 5');
-      assert.deepEqual([5,5], grid);
+describe('Mars Rover Challenge', function () {
+  describe('Parse Input', function () {
+    it('should correctly parse the grid', function () {
+      let [err, grid] = mars.parseGrid('5 5');
+      assert.deepEqual(grid, [5, 5]);
     })
   })
 
+  describe('Parse rover details', function () {
+    it('should correct parse rover details', function () {
+      let [err, rover] = mars.getRoverPosition('1 2 N');
+      assert.deepEqual(rover, { x: 1, y: 2, dir: 'N' })
+    })
+  })
 
-  // describe('Final Tests', function() {
-  //   it(`should return \n${testoutput1} \nfrom \n${testinput1}`, function() {
-  //     assert.equal(testoutput1, mars.readInput(testinput1));
-  //   });
+  describe('Move the rover', function () {
+    let testSize = [5, 5];
+    let testRover = { x: 1, y: 2, dir: 'N' };
+    let testMoves = 'LMLMLMLMM';
 
-  //   it(`should return \n${testoutput2} \nfrom \n${testinput2}`, function() {
-  //     assert.equal(testoutput2, mars.readInput(testinput2));
-  //   });
-  // });
+    it('should correctly move the rover around the gird', function () {
+      let [err, rover] = mars.doRoverMoves(testSize, testRover, testMoves);
+      assert.deepEqual(rover, { x: 1, y: 3, dir: 'N' });
+    })
+
+    it('should fail when given an invalid rover', function () {
+      let [err, rover] = mars.doRoverMoves(testSize, null, testMoves);
+      assert.equal(err, 'Error: doRoverMoves given null as rover');
+    })
+
+    it('should fail when given invalid moves', function () {
+      let [err, rover] = mars.doRoverMoves(testSize, testRover, null);
+      assert.equal(err, 'Error: doRoverMoves given null for moves');
+    })
+
+  })
+
+  describe('Final Tests', function () {
+    it(`should return \n${testoutput1} \nfrom \n${testinput1}`, function () {
+      assert.equal(mars.readInput(testinput1), testoutput1);
+    });
+
+    // it(`should return \n${testoutput2} \nfrom \n${testinput2}`, function() {
+    //   assert.equal(testoutput2, mars.readInput(testinput2));
+    // });
+  });
 });
